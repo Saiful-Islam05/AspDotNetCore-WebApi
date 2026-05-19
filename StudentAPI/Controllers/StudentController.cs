d3c406e38c0564b84dd59fbd37044c73c0e23588
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-05-19 10:01:34 +0600

    Topic 1: Foundation & Project Setup

---
+++ StudentAPI/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        // Get: api/Student
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = new List<object>
+            {
+                new { Id = 1, Name = "Rahim", Age = 20 },
+                new { Id = 2, Name = "Karim", Age = 22 },
+                new { Id = 3, Name = "Jabbar", Age = 21 },
+                new { Id = 4, Name = "Shafiq", Age = 23 }
+
+            };
+
+            return Ok(students);  // 200 Status Code with the list of students
+        }
+
+        // Get: api/Student/1
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            var student = new { Id = id, Name = "Rahim", Age = 20, City = "Dhaka" };
+            return Ok(student);
+        }
+    }
+}
