49594bf032e5c4fdea374ac5ee5bd64fe0e18e94
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-05-31 12:05:45 +0600

    Topic 2 → Controller & Action Method

--- StudentAPI/Controllers/StudentController.cs
+++ StudentAPI/Controllers/StudentController.cs
@@ -4,7 +4,7 @@ namespace StudentAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StudentController : Controller
+    public class StudentController : ControllerBase
     {
         // Get: api/Student
         [HttpGet]
@@ -28,6 +28,30 @@ namespace StudentAPI.Controllers
         {
             var student = new { Id = id, Name = "Rahim", Age = 20, City = "Dhaka" };
             return Ok(student);
+        }
+
+        // New - POST (student creation)
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody] string name)
+        {
+            // In a real application, we will save the student from the database. so give fake response
+            var newStudent = new { Id = 5, Name = name, Age = 20 };
+            return Created("", newStudent); // 201 Status Code with the created student
+        }
+
+        // New - PUT (student update)
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, [FromBody] string newName)
+        {
+            var updated = new { Id = id, Name = newName, Message = "Updated Successfully!" };
+            return Ok(updated); // 200 Status Code with the updated student
+        }
+
+        // New - DELETE (student deletion)
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"Student {id} deleted successfully!"); // 200 Status Code with a deletion message
         }
     }
 }
