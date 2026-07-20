1991476444839e9b850bbd656cab7b636996461d
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-07-20 20:39:07 +0600

    CourseController work start

---
+++ StudentAPI/Controllers/CourseController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAPI.Repositories;
+using StudentAPI.Models;
+
+namespace StudentAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _repository;
+
+        public CourseController(ICourseRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // =====================================================
+        // ✅ GET ALL — সব Course আনো
+        // URL: GET /api/course
+        // =====================================================
+
+        [HttpGet]
+        public async Task<ActionResult<List<CourseResponseDTO>>> GetAllCourses()
+        {
+            var courses = await _repository.GetAllAsync();
+
+            // Course → CourseResponseDTO convert
+            var response = courses.Select(c => new CourseResponseDTO
+            {
+                Id = c.Id,
+                Title = c.Title,
+                Description = c.Description,
+                CreditHours = c.CreditHours,
+                CreatedAt = c.CreatedAt
+            }).ToList();
+
+            return Ok(response);
+        }
+
+
+        // =====================================================
+        // ✅ GET BY ID — একটা Course আনো
+        // URL: GET /api/course/1
+        // =====================================================
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CourseResponseDTO>> GetCourseById(int id)
+        {
+            var course = await _repository.GetByIdAsync(id);
+            if (course == null)
+            {
+                return NotFound(new { Message = $"ID {id}  Course not found" });
+            }
+
+            var response = new CourseResponseDTO
+            {
+                Id = course.Id,
+                Title = course.Title,
+                Description = course.Description,
+                CreditHours = course.CreditHours,
+                CreatedAt = course.CreatedAt
+            };
+
+            return Ok(response);
+        }
+
+
+        // =====================================================
+        // ✅ SEARCH — Title দিয়ে Course খোঁজো
+        // URL: GET /api/course/search?title=csharp
+        // =====================================================
+        [HttpGet("search")]
+        public async Task<ActionResult<List<CourseResponseDTO>>> SearchCourses([FromQuery] string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                return BadRequest(new { Message = "Title query parameter is required." });
+            }
+
+
+            var courses = await _repository.SearchByTitleAsync(title);
+
+            if (courses.Count == 0)
+                return NotFound(new { Message = $"'{title}' by this not find any course" });
+
+            // Course → CourseResponseDTO convert
+            var response = courses.Select(c => new CourseResponseDTO
+            {
+                Id = c.Id,
+                Title = c.Title,
+                Description = c.Description,
+                CreditHours = c.CreditHours,
+                CreatedAt = c.CreatedAt
+            }).ToList();
+            return Ok(response);
+        }
+
+
+        
+        // =====================================================
+        // ✅ POST — নতুন Course তৈরি
+        // URL: POST /api/course
+        // =====================================================
+
+    }
+}
---
+++ StudentAPI/Migrations/20260720140952_AddCourseTable.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCourseTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreditHours = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "CreatedAt", "CreditHours", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2026, 7, 20, 20, 9, 51, 665, DateTimeKind.Local).AddTicks(1125), 0, "This is a C# programming course.", "C# Programming" },
+                    { 2, new DateTime(2026, 7, 20, 20, 9, 51, 665, DateTimeKind.Local).AddTicks(1127), 0, "This is an ASP.NET Web API course.", "Asp Dot net Web API" },
+                    { 3, new DateTime(2026, 7, 20, 20, 9, 51, 665, DateTimeKind.Local).AddTicks(1128), 0, "This is a SQL Server course.", "SQL Server" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 7, 20, 20, 9, 51, 665, DateTimeKind.Local).AddTicks(951));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 7, 20, 20, 9, 51, 665, DateTimeKind.Local).AddTicks(962));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 7, 20, 20, 9, 51, 665, DateTimeKind.Local).AddTicks(964));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 7, 9, 20, 11, 14, 453, DateTimeKind.Local).AddTicks(1084));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 7, 9, 20, 11, 14, 453, DateTimeKind.Local).AddTicks(1094));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 7, 9, 20, 11, 14, 453, DateTimeKind.Local).AddTicks(1095));
+        }
+    }
+}
